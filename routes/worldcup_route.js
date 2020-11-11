9fade59994f860d46363f46a5ecb8e85be82d9b9
iamhge <fine632@gmail.com>
2020-11-12 02:49:09 +0900

    Upgrade ideal_worldcup to select professors

--- routes/worldcup_route.js
+++ routes/worldcup_route.js
@@ -4,15 +4,32 @@ var worldcup  = function(req, res) {
     res.render('./ideal_worldcup/worldcup.ejs', {member: req.user});
 }
 
-var result = function(req, res) {
+var result =  function(req, res) {
     console.log("===== Router Call =====");
     console.log("Router : result");
     res.render('./ideal_worldcup/result.ejs', {member: req.user});
 }
 
+// var express = require('express');
+// var professorList = express();
+
+// professorList.use(express.json());
+
+// professorList.get('/', function(req, res) {
+//     var professorList = require("./professors.json");
+//     res.json(professorList);
+// })
+
+var professorList = function(req, res) {
+    var professorList = require("./professors.json");
+    res.json(professorList);
+    console.log(professorList);
+}
+
 var route_func = {
     worldcup: worldcup,
-    result: result
+    result: result,
+    professorList: professorList
 }
 
 module.exports = route_func;
